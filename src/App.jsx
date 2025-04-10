53b115e87a7e04e92b6ba9162eeb298cb369fe79
Vijay Reshamwala <vijay.reshamwala@artoon.in>
2025-04-10 15:31:57 +0530

    changes main file

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <h1>Hello world</h1>
+     <h1>Hello world 123</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
